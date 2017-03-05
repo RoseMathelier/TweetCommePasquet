138a263668b46be8d76789fcd8aa06f373b1c944
RoseMathelier <rose.mathelier@gmail.com>
2017-03-05 20:41:40 +0100

    débuggage du hello_text et centrage de l'image

--- ajax_iss.js
+++ ajax_iss.js
@@ -8,15 +8,17 @@ var marker;
 var zoom;
 var coord;
 
+var color;
 var points = new Array(); //initialisation du tableau qui servira à tracer les polylignes
 points.push([]); // dans ce tableau on ajoute un tableau vide qui correspond à la première polyligne
 var nb_polylines = 1;
+var trajet_iss;
 
 var location_name;
 var country;
 
 var photo;
-var text_loc;
+var hello_para;
 
 var first_time = new Date().getTime();
 
@@ -72,10 +74,10 @@ function addPolyline(map,coord,points,nb_polylines){
 	//Tracé de la (ou des) polyligne(s)s
 	for(var i = 0; i < nb_polylines; i++){
 	  if(points[i].length > 1){ //on commence à tracer la polyligne dès qu'on a + de 2 points.
-		var trajet_iss = new google.maps.Polyline({
+		trajet_iss = new google.maps.Polyline({
 		  path: points[i],
 		  geodesic: true,
-		  strokeColor: '#FF0000',
+		  strokeColor: color,
 		  strokeOpacity: 1.0,
 		  strokeWeight: 2
 		});
@@ -156,8 +158,11 @@ function getGeoname() {
 		
 		//Cas où l'on est sur un continent
 		if(result.hasOwnProperty("geoname")){
-			location_name = result[geonames.length - 1].name;
-			country = result[2].name;
+			var tab = result.geoname;
+			var len = tab.length;
+			var geo = tab[len-1];
+			location_name = geo.name;
+			country = geo.countryName;
 		}
 		
 		//Cas où l'on est sur un océan
@@ -171,17 +176,19 @@ function getGeoname() {
 		}
 
         //Ajout du texte dans le DOM
-        var hello_para = document.createElement('p');
-        if(typeof(text_loc) != "undefined"){ //si le texte existe déjà, on le supprime.
-          hello_para.removeChild(text_loc);
+		var espace_text = document.getElementById("hello_tweet");
+        if(typeof(hello_para) != "undefined"){ //si le texte existe déjà, on le supprime.
+          espace_text.removeChild(hello_para);
         };
-        text_loc = "Hello, "+location_name+" !";
+		hello_para = document.createElement('p');
+        var hello_text = "Hello "+location_name;
         if(typeof(country) != "undefined"){ //si on n'est pas au dessus de l'océan, donc s'il y a un nom de pays à ajouter, on l'ajoute.
-          text_loc = text_loc+", "+country;
+          hello_text = hello_text + ", " +country;
         };
-        var hello_text = document.createTextNode(text_loc);
-        hello_para.appendChild(hello_text);
-        document.getElementById("hello_tweet").appendChild(hello_para);
+		hello_text = hello_text + " !"
+        var tweet = document.createTextNode(hello_text);
+        hello_para.appendChild(tweet);
+        espace_text.appendChild(hello_para);
       }
   };
 
@@ -194,15 +201,36 @@ function getPositionAPI () {
 	
 	var ajax2 = new XMLHttpRequest();
 	var url2 = "api_loc.php?time="+first_time.toString();
-	ajax.onreadystatechange = function() {
+	ajax2.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
 		  
-		  //récupération des données
+		//Récupération des coordonnées
+        var result_api = this.responseText;
+		console.log(result_api);
+		var result_api_json = JSON.parse(result_api);
+		lat_iss = result_api_json.latitude;
+        long_iss = result_api_json.longitude;
+        coord = {lat: lat_iss, lng: long_iss};
+		
+		//Mise à jour de la carte avec les données récupérées
+        if(document.getElementById("c_follow_iss").checked){
+          map.setCenter(coord);
+        };
+
+        //Mise à jour de la zone texte indiquant les coordonnées de l'ISS
+        textPosition(lat_iss, long_iss);
+
+        //Ajout des markers
+		addMarker(map,coord);
+
+        //Tracé de la polyligne
+		addPolyline(map,coord,points,nb_polylines);
+		
 		  
 	  }
 	}
-	ajax.open("GET", url2, true);
-	ajax.send();
+	ajax2.open("GET", url2, true);
+	ajax2.send();
 	
 	
 }
@@ -214,10 +242,33 @@ function getPositionAPI () {
 initMap(0,0,2);
 textPosition(0,0);
 
-//Event listener sur la case d'activation du mode debug
+//Mise en place de l'intervalle en mode normal (non debug)
+color = '#FF0000';
+interval = setInterval(getPosition, 5000);
 
-//Mise à jour toutes les 5 secondes pour suivre la position de l'ISS.
-setInterval(getPosition, 5000);
+
+//Event listener sur la checkbox debug;
+document.getElementById("debug").addEventListener("click", function(event){
+	
+
+	
+	//Mise à jour toutes les 5 secondes pour suivre la position de l'ISS
+	if(document.getElementById("debug").checked){
+		if(typeof(interval) != "undefined"){
+			clearInterval(interval); //si un intervalle existe déjà, on le supprime
+		}
+		color = '#32CD32';
+		interval = setInterval(getPositionAPI, 5000); //mode debug
+	}
+	else {
+		if(typeof(interval) != "undefined"){
+			clearInterval(interval); //si un intervalle existe déjà, on le supprime
+		}
+		color = '#FF0000';
+		interval = setInterval(getPosition, 5000); //mode normal
+	}
+	
+});
 
 //Event listener sur le bouton Tweet comme Pesquet
 document.getElementById("validation").addEventListener("click", function(event){
--- api_loc.php
+++ api_loc.php
@@ -10,15 +10,20 @@ $altitude_iss = 400; // en km
 $inclinaison_iss = 51.64*M_PI/180; //en radian
 $polar = M_PI/2; //en radian
 
+//Coordonnées de l'ellipsoide
+$e = 0.08181919106;
+$a = 6378137.06356752314;
+$f = 1 - sqrt(1-$e**2);
+
 //On récupère le temps écoulé depuis la 1ère connexion (en ms, puis en h)
 $first_time = $_GET["time"];
-$current_time = microtime(true)*1000
-$delta_t = $current_time - $first_time //en ms
+$current_time = microtime(true)*1000;
+$delta_t = $current_time - $first_time; //en ms
 $delta_t_h = $delta_t /(1000*3600); //en h
 
 //Calcul de l'azimuth de la station
 $perimetre_orbite = 2 * M_PI * ($rayon_terre + $altitude_iss); //en km
-$distance_parcourue = $vitesse_iss * $delta_t_h //en km
+$distance_parcourue = $vitesse_iss * $delta_t_h; //en km
 $azimuth = $distance_parcourue * 2 * M_PI / $perimetre_orbite; // en radian
 
 //Coordonnées (X,Y,Z) du point
@@ -31,12 +36,29 @@ $X_new = $X_init * cos($inclinaison_iss) - $Z_init * sin($inclinaison_iss);
 $Y_new = $Y_init;
 $Z_new = $X_init * sin($inclinaison_iss) + $Z_init * cos($inclinaison_iss);
 
-//Calcul de la latitude et de la longitude du point
-$latitude_iss = 0;
-$longitude_iss = 0;
+//Calcul de la latitude
+$mu = atan(($Z_new/(sqrt($X_new**2+$Y_new**2))) * ((1-$f) + ($e**2 * $a / $rayon_terre)));
+$latitude_iss = atan(($Z_new*(1-$f) + $e**2 * $a *(sin($mu))**3) / ((1-$f)*(sqrt($X_new**2+$Y_new**2) - $e**2 * $a * (cos($mu))**3 )));
+
+//Calcul de la longitude
+if($X_new > 0){
+	$longitude_iss = atan($Y_new/$X_new);
+}
+else if($Y_new >= 0 and $X_new < 0){
+	$longitude_iss = atan($Y_new/$X_new) + M_PI;
+}
+else if($Y_new < 0 and $X_new < 0){
+	$longitude_iss = atan($Y_new/$X_new) - M_PI;
+}
+else if($Y_new > 0 and $X_new = 0){
+	$longitude_iss = M_PI/2;
+}
+else if($Y_new < 0 and $X_new = 0){
+	$longitude_iss = - M_PI/2;
+}	
 
 //Retour des coordonnées en JSON
-$coord = '{"lat": '.$latitude_iss.', "long": '.$longitude_iss.'}';
+$coord = '{"latitude": '.$latitude_iss.', "longitude": '.$longitude_iss.'}';
 echo $coord;
 
 ?>
